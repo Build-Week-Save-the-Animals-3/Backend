7955dbf8bcb664837862786d50150026231b696a
Danielle B <danibaxx@hotmail.com>
2020-01-30 16:57:53 -0700

    migrated test/dev db, started creating seeds, cleanup created

---
+++ data/seeds/00_cleanup.js
@@ -0,0 +1,11 @@
+
+exports.seed = async (knex) => {
+  await knex("orgs_camps").truncate()  
+  await knex("camps_animals").truncate()
+  await knex("sups_donations").truncate()
+  await knex("donations").truncate()
+  await knex("supporters").truncate()
+  await knex("campaigns").truncate()
+  await knex("species").truncate()
+  await knex("organizations").truncate()
+}
