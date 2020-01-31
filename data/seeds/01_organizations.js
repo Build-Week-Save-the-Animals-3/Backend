36223b7b83e47afcdad1e5a6feb37f36064a0e35
Danielle B <danibaxx@hotmail.com>
2020-01-30 17:30:32 -0700

    seed organizations and species with data

---
+++ data/seeds/01_organizations.js
@@ -0,0 +1,9 @@
+
+exports.seed = async function(knex) {
+  await knex('organizations').insert([
+    { name: "Best Friends First" },
+    { name: "Fish Are Friends, Not Food" },
+    { name: "Koala Sanctuary" },
+    { name: "All Claws and Paws" },
+  ])
+};
---
+++ data/seeds/02_species.js
@@ -0,0 +1,18 @@
+
+exports.seed = async function(knex) {
+  await knex('species').insert([
+    { name: "river otter", org_id: 1 },
+    { name: "polar bear", org_id: 1 },
+    { name: "red panda", org_id: 1 },
+    { name: "seahorse", org_id: 2 },
+    { name: "sea turtle", org_id: 2 },
+    { name: "sea otter", org_id: 2 },
+    { name: "dugong", org_id: 2 },
+    { name: "koala", org_id: 3 },
+    { name: "bilbies", org_id: 3 },
+    { name: "tarantula", org_id: 4 },
+    { name: "mouse lemur", org_id: 4 },
+    { name: "orangutan", org_id: 4 },
+    { name: "snow leopard", org_id: 4 },
+  ])
+};
