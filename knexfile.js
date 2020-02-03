06d2c12043428cfd7099ac3ae25dff72cfe6718e
Danielle B <danibaxx@hotmail.com>
2020-02-02 18:58:08 -0700

    updated knexfile.js to deploy production app

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,24 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const sqlite = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+  pool : {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    }
+  },
+};
 
 module.exports = {
   production: {
-    client: "pg",
+    client: "sqlite3",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
