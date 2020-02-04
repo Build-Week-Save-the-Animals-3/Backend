0326e69e9ecf8b85129ea60b75daf56dc7ff87c0
Danielle B <danibaxx@hotmail.com>
2020-02-04 16:40:33 -0700

    changed up cleanup file

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,10 +1,4 @@
 
 exports.seed = async (knex) => {
-  await knex("camps_animals").truncate()
-  await knex("sups_donations").truncate()
-  await knex("donations").del()
-  await knex("supporters").truncate()
-  await knex("campaigns").truncate()
-  await knex("species").truncate()
-  await knex("organizations").truncate()
+  await knex.raw("truncate camps_animals, sups_donations, donations, supporters, campaigns, species, organizations restart identity cascade")
 }
