085adea9d5dd4dc6662e1df7f860a28786b66703
Danielle B <danibaxx@hotmail.com>
2020-02-04 20:24:44 -0700

    trying to figure out why my post fx aren't working correctly upon testingin insomnia

--- campaigns/campaigns-model.js
+++ campaigns/campaigns-model.js
@@ -3,67 +3,67 @@ const db = require('../data/db-config');
 // view, edit, delete existing campaigns - orgs
 // make donation to campaigns, find campaigns - supporters
 function findCampaigns() {
-  const camps = db('campaigns').select()
-  return camps.map(camp => {
-    return {
-      ...camp,
-      completed: camp.completed === 0 ? false : true
-    };
-  })
-};
+	const camps = db('campaigns').select();
+	return camps.map(camp => {
+		return {
+			...camp,
+			completed: camp.completed === 0 ? true : false,
+		};
+	});
+}
 
 function findOrganizations() {
-  return db('organizations').select()
-};
+	return db('organizations').select();
+}
 
 function findDonations() {
-  return db('donations').select()
-};
+	return db('donations').select();
+}
 
 function findCampById(id) {
-  return db('campaigns')
-    .where({ id })
-    .first()
-};
+	return db('campaigns')
+		.where({ id })
+		.first();
+}
 
 function findDonsById(id) {
-  return db('donations')
-    .where({ id })
-    .first()
-};
+	return db('donations')
+		.where({ id })
+		.first();
+}
 
 async function addCampaigns(campaign) {
-  const [id] = await db('campaigns').returning(campaign)
-  return findCampById(id)
-};
+	const [id] = await db('campaigns').insert(campaign);
+	return findCampById(id);
+}
 
 async function addDonations(donate) {
-  const [id] = await db('donations').returning(donate)
-  return findDonsById(id)
-};
+	const [id] = await db('donations').insert(donate);
+	return findDonsById(id);
+}
 
 async function updateCampaign(id, changes) {
-  await db('campaigns')
-    .where({ id })
-    .update(changes)
+	await db('campaigns')
+		.where({ id })
+		.update(changes);
 
-  return findCampById(id)
-};
+	return findCampById(id);
+}
 
 function deleteCampaign(id) {
-  return db('campaigns')
-    .where({ id })
-    .del()
-};
+	return db('campaigns')
+		.where({ id })
+		.del();
+}
 
 module.exports = {
-  findCampaigns,
-  findOrganizations,
-  findDonations,
-  findCampById,
-  findDonsById,
-  addCampaigns,
-  addDonations,
-  updateCampaign,
-  deleteCampaign,
+	findCampaigns,
+	findOrganizations,
+	findDonations,
+	findCampById,
+	findDonsById,
+	addCampaigns,
+	addDonations,
+	updateCampaign,
+	deleteCampaign,
 };
